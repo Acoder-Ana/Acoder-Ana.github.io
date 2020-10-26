c69501e6db53eb86f68f706a612ade7aecfea792
Acoder-Ana <73392904+Acoder-Ana@users.noreply.github.com>
2020-10-26 14:15:28 +0300

    first commit
    Pofile Site

---
+++ precache-manifest.2f5a1b2e537d4af91c03724230ebf5c5.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cde3d60394771d2e15849acbc1edffa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67dee96ad15c47a2231a",
+    "url": "/static/css/main.169834a5.chunk.css"
+  },
+  {
+    "revision": "58b1fc92c6af29229b16",
+    "url": "/static/js/2.5591e48d.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/static/js/2.5591e48d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67dee96ad15c47a2231a",
+    "url": "/static/js/main.9f316996.chunk.js"
+  },
+  {
+    "revision": "0a5ef299e2ccd22efa04",
+    "url": "/static/js/runtime-main.3899a15a.js"
+  },
+  {
+    "revision": "9c6a311406f71db600b5818d4f680331",
+    "url": "/static/media/classic-about.9c6a3114.svg"
+  },
+  {
+    "revision": "141d3b2282a41315173cc7215a722782",
+    "url": "/static/media/classic-edu.141d3b22.svg"
+  },
+  {
+    "revision": "7e19eab305b630026be0f8f561a01730",
+    "url": "/static/media/classic-settings.7e19eab3.svg"
+  },
+  {
+    "revision": "386e758779d5ea2bbe757f31873b7dd9",
+    "url": "/static/media/classic-skills.386e7587.svg"
+  },
+  {
+    "revision": "14492fe1ce6da28731f550ece1a1fd64",
+    "url": "/static/media/classic-work.14492fe1.svg"
+  },
+  {
+    "revision": "cd3014db9f7a85ab58c79bd568b460da",
+    "url": "/static/media/flat-about.cd3014db.svg"
+  },
+  {
+    "revision": "1e136508a4073a95f3f3b6dd35cd0eca",
+    "url": "/static/media/flat-edu.1e136508.svg"
+  },
+  {
+    "revision": "788d68a604d7fcdab971078d9fa63ce7",
+    "url": "/static/media/flat-settings.788d68a6.svg"
+  },
+  {
+    "revision": "9eec3a40c70bf62fe4eeb3e9f16100dc",
+    "url": "/static/media/flat-skills.9eec3a40.svg"
+  },
+  {
+    "revision": "eec1ccf5a0453840ef23d93e57259aae",
+    "url": "/static/media/flat-work.eec1ccf5.svg"
+  },
+  {
+    "revision": "1d20d8376530d161a7bc32812b103efb",
+    "url": "/static/media/folder-front.1d20d837.svg"
+  },
+  {
+    "revision": "c6cb0badfa072bb3bfc8a2325b2a3d1a",
+    "url": "/static/media/logo-bosch.c6cb0bad.webp"
+  },
+  {
+    "revision": "9b1c8681f446aecb03ffb1f344667a17",
+    "url": "/static/media/logo-cathay.9b1c8681.webp"
+  },
+  {
+    "revision": "2a34bfd2c34b3be4ecbbb1b6512dfcc2",
+    "url": "/static/media/logo-fandom-dev.2a34bfd2.webp"
+  },
+  {
+    "revision": "cfb604535e1e22dd75e504261a087074",
+    "url": "/static/media/logo-fandom.cfb60453.webp"
+  },
+  {
+    "revision": "8152e44195f2dafea03001cb3cbe9190",
+    "url": "/static/media/logo-pela.8152e441.webp"
+  },
+  {
+    "revision": "8ee9b8f876a0aa5ea47217b75d0b50fe",
+    "url": "/static/media/logo-shaw-ca.8ee9b8f8.svg"
+  },
+  {
+    "revision": "2867b9e445e526aa4b8f5f7dbbf3a23c",
+    "url": "/static/media/logo-win7.2867b9e4.webp"
+  },
+  {
+    "revision": "cf4398833bfc1e41ffafbfd450636a70",
+    "url": "/static/media/skill-androidstd.cf439883.svg"
+  },
+  {
+    "revision": "500f856861956c3017d634f727e396fa",
+    "url": "/static/media/skill-balsamiq.500f8568.svg"
+  },
+  {
+    "revision": "021b9945a4e4e39ac7e7e6b95afc573b",
+    "url": "/static/media/skill-bootstrap.021b9945.svg"
+  },
+  {
+    "revision": "7037158cd999184892f98fc2488915db",
+    "url": "/static/media/skill-csharp.7037158c.svg"
+  },
+  {
+    "revision": "e2055856cabe3cbe506714c11292ef6f",
+    "url": "/static/media/skill-css.e2055856.svg"
+  },
+  {
+    "revision": "70d16b8555b8ff8b1b452ed85e76b86f",
+    "url": "/static/media/skill-eclipse.70d16b85.svg"
+  },
+  {
+    "revision": "f618641c78751b81086e2cd729ddb296",
+    "url": "/static/media/skill-html.f618641c.svg"
+  },
+  {
+    "revision": "84321945474161660fa7d892460c2d6c",
+    "url": "/static/media/skill-java.84321945.svg"
+  },
+  {
+    "revision": "89f468496a08a3d43503fd3eae3ceaf1",
+    "url": "/static/media/skill-jquery.89f46849.svg"
+  },
+  {
+    "revision": "f2bd0c0cb064f241f93ab02be9e524b0",
+    "url": "/static/media/skill-js.f2bd0c0c.svg"
+  },
+  {
+    "revision": "bd03a0da3698c796d747debcb3d81725",
+    "url": "/static/media/skill-office.bd03a0da.svg"
+  },
+  {
+    "revision": "fa8d8fac491a323f92674a521a38d401",
+    "url": "/static/media/skill-photoshop.fa8d8fac.svg"
+  },
+  {
+    "revision": "44403f1eece67c892a0c6a6791da5ad4",
+    "url": "/static/media/skill-php.44403f1e.svg"
+  },
+  {
+    "revision": "1a36cc4d7f1b283edce389cb41bddb1f",
+    "url": "/static/media/skill-reactjs.1a36cc4d.svg"
+  },
+  {
+    "revision": "e0f651ac7aecf9187f66e5bd5f181c10",
+    "url": "/static/media/skill-sass.e0f651ac.svg"
+  },
+  {
+    "revision": "511e3b5a6255bf6159837d1b007a4c3c",
+    "url": "/static/media/skill-vs.511e3b5a.svg"
+  },
+  {
+    "revision": "2a1a2bebce77c639a461658a0da9e827",
+    "url": "/static/media/skill-vscode.2a1a2beb.svg"
+  },
+  {
+    "revision": "6fb1734b4f058fb95c0bcff327d69bae",
+    "url": "/static/media/skill-vuejs.6fb1734b.svg"
+  },
+  {
+    "revision": "87276c977499238ebccfdbd6f6792e6c",
+    "url": "/static/media/social-fandom-active.87276c97.webp"
+  },
+  {
+    "revision": "d9c4838a33bffb6c2cc6e41655c01671",
+    "url": "/static/media/social-fandom.d9c4838a.webp"
+  },
+  {
+    "revision": "d2183868347510172945d80e1becede2",
+    "url": "/static/media/social-github-active.d2183868.webp"
+  },
+  {
+    "revision": "861a9ee60dbd26d98fda80d001e65079",
+    "url": "/static/media/social-github.861a9ee6.webp"
+  },
+  {
+    "revision": "d3c4472c4bd5228e3ae27a8211c1f46c",
+    "url": "/static/media/social-linkedin-active.d3c4472c.webp"
+  },
+  {
+    "revision": "9d4bd073158cfbed48490fae64d29163",
+    "url": "/static/media/social-linkedin.9d4bd073.webp"
+  },
+  {
+    "revision": "790dc8edb6b29513f891196578810a5b",
+    "url": "/static/media/theme-classic.790dc8ed.webp"
+  },
+  {
+    "revision": "5741994acd864ba4ba2cc86af509f7f7",
+    "url": "/static/media/theme-flat.5741994a.webp"
+  },
+  {
+    "revision": "4ac429dbf6eb53c9d5f9e3642121696c",
+    "url": "/static/media/theme-neumorph.4ac429db.webp"
+  }
+]);
